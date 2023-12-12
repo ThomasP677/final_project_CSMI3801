408a66497840b665d977f79f7454105355f21319
ThomasP677 <85772082+ThomasP677@users.noreply.github.com>
2023-12-11 16:54:37 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -31,11 +31,23 @@ class ScrapedData:
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36", 
             "X-Amzn-Trace-Id": "Root=1-6568f7cc-1700f23442d59a687e8e741e"
         }
-        page = requests.get(url, headers)
+        page = requests.get(url,headers)
 
+        word = "Ravel"
+        list_header = []
         soup1 = BeautifulSoup(page.content, "html.parser")
         soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
 
-        title = soup2.find('script').get_text()
+        for tag in soup1.find_all():
+            list_header.append(BeautifulSoup(tag,'html.parser'))
+            
+        for header in list_header:
+            for text in header.get_text():
+                if word in text:
+                    return text
+
+        s = soup1.find(tag).get_text()
         price = title.find('priceblock_ourprice').get_text()
         title = title.strip()
+        print(soup2)
+        print(list_header)
